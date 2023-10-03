82c1d93c37443360853305f812bbec99dcc3280a
vivannikov1 <vivannikov@neoflex.ru>
2023-10-03 11:01:21 +0300

    handle min total and current experience

--- deal/src/main/java/com/neoflex/creditconveyer/deal/domain/constant/Constants.java
+++ deal/src/main/java/com/neoflex/creditconveyer/deal/domain/constant/Constants.java
@@ -2,4 +2,7 @@ package com.neoflex.creditconveyer.deal.domain.constant;
 
 public class Constants {
     public static final Integer AGE_ADULT = 18;
+
+    public static final Integer MIN_VALID_TOTAL_EXPERIENCE = 12;
+    public static final Integer MIN_VALID_CURRENT_EXPERIENCE = 3;
 }
--- deal/src/main/java/com/neoflex/creditconveyer/deal/validation/validator/EmploymentValidator.java
+++ deal/src/main/java/com/neoflex/creditconveyer/deal/validation/validator/EmploymentValidator.java
@@ -1,6 +1,8 @@
 package com.neoflex.creditconveyer.deal.validation.validator;
 
+import com.neoflex.creditconveyer.deal.domain.constant.Constants;
 import com.neoflex.creditconveyer.deal.domain.dto.EmploymentDTO;
+import com.neoflex.creditconveyer.deal.error.exception.BadRequestException;
 import com.neoflex.creditconveyer.deal.error.exception.ResourceNotFoundException;
 import com.neoflex.creditconveyer.deal.validation.constraint.EmploymentConstraint;
 import jakarta.validation.ConstraintValidator;
@@ -27,6 +29,13 @@ public class EmploymentValidator implements ConstraintValidator<EmploymentConstr
         }
         if (null == employment.getWorkExperienceCurrent()) {
             throw new ResourceNotFoundException("Invalid value. workExperienceCurrent shouldn't be null");
+        }
+
+        if (employment.getWorkExperienceTotal() < Constants.MIN_VALID_TOTAL_EXPERIENCE) {
+            throw new BadRequestException(String.format("Invalid value. Total experience might not be less than %d months.", Constants.MIN_VALID_TOTAL_EXPERIENCE));
+        }
+        if (employment.getWorkExperienceCurrent() < Constants.MIN_VALID_CURRENT_EXPERIENCE) {
+            throw new BadRequestException(String.format("Invalid value. Current experience might not be less than %d months.", Constants.MIN_VALID_CURRENT_EXPERIENCE));
         }
 
         log.debug("Response, validation EmploymentDTO for null is finished");
