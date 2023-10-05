50e92fb4fed3cb011ef41bf0d244ea939a52e4e0
vivannikov1 <vivannikov@neoflex.ru>
2023-10-05 11:39:25 +0300

    init custom @AdultConstraint

--- application/src/main/java/com/neoflex/creditconveyer/application/domain/dto/LoanApplicationRequestDTO.java
+++ application/src/main/java/com/neoflex/creditconveyer/application/domain/dto/LoanApplicationRequestDTO.java
@@ -1,6 +1,9 @@
 package com.neoflex.creditconveyer.application.domain.dto;
 
+import com.neoflex.creditconveyer.application.validation.constraint.AdultConstraint;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,17 +14,31 @@ import java.time.LocalDate;
 @Setter
 public class LoanApplicationRequestDTO {
     @NotNull
+    @Min(10000)
     private BigDecimal amount;
     @NotNull
+    @Min(6)
     private Integer term;
 
     @NotNull
+    @FirstLastMiddleNameConstraint
     private String firstName;
+    @NotNull
+    @FirstLastMiddleNameConstraint
     private String lastName;
+    @FirstLastMiddleNameConstraint
     private String middleName;
+
+    @Email(regexp = "[\\w\\.]{2,50}@[\\w\\.]{2,20}")
     private String email;
+
+    @AdultConstraint
     private LocalDate birthdate;
+
+    @NotNull
+    @Size(min = 4, max = 4)
     private String passportSeries;
+    @NotNull
+    @Size(min = 6, max = 6)
     private String passportNumber;
 }
-
---
+++ application/src/main/java/com/neoflex/creditconveyer/application/validation/validator/AdultValidator.java
@@ -0,0 +1,32 @@
+package com.neoflex.creditconveyer.application.validation.validator;
+
+import com.neoflex.creditconveyer.application.error.exception.ResourceNotFoundException;
+import com.neoflex.creditconveyer.application.validation.constraint.AdultConstraint;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDate;
+
+@Slf4j
+public class AdultValidator implements ConstraintValidator<AdultConstraint, LocalDate> {
+
+    @Override
+    public void initialize(AdultConstraint constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(LocalDate birthday, ConstraintValidatorContext constraintValidatorContext) {
+        log.debug("Request, validation birthday. birthday={}", birthday);
+
+        if (null == birthday) {
+            log.error("Invalid value. Birthday shouldn't be null");
+            throw new ResourceNotFoundException("Invalid value. Birthday shouldn't be null");
+        }
+        Integer years = DatesUtil.getYears(birthday);
+
+        log.debug("Response, validation birthday. years={}", years);
+        return years >= Constants.AGE_ADULT;
+    }
+}
