23964cfb9296c9f21f8a046b79e12ae5490724fe
vivannikov1 <vivannikov@neoflex.ru>
2023-10-06 12:06:13 +0300

    sender email

---
+++ dossier/src/main/java/com/neoflex/creditconveyer/dossier/DossierApp.java
@@ -0,0 +1,12 @@
+package com.neoflex.creditconveyer.dossier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DossierApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DossierApp.class, args);
+    }
+}
---
+++ dossier/src/main/java/com/neoflex/creditconveyer/dossier/config/DossierConfig.java
@@ -0,0 +1,42 @@
+package com.neoflex.creditconveyer.dossier.config;
+
+import com.neoflex.creditconveyer.dossier.utils.CommonUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.io.IOException;
+import java.util.Properties;
+
+@Configuration
+public class DossierConfig {
+
+    @Value("${mail.host}")
+    private String host;
+    @Value("${mail.port}")
+    private Integer port;
+    @Value("${mail.email}")
+    private String email;
+    @Value("${mail.password}")
+    private String password;
+
+    @Bean
+    public MailSender mailSender() throws IOException {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(email);
+        mailSender.setPassword(password);
+
+        Properties properties = new Properties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.debug", "true");
+
+        mailSender.setJavaMailProperties(properties);
+        return mailSender;
+    }
+}
