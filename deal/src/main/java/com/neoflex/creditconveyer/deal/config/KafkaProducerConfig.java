9bc44bb95e949f70b8c877f47dfc79364ba28330
vivannikov1 <vivannikov@neoflex.ru>
2023-10-06 12:53:09 +0300

    add KafkaProducer configurations

---
+++ deal/src/main/java/com/neoflex/creditconveyer/deal/config/KafkaProducerConfig.java
@@ -0,0 +1,29 @@
+package com.neoflex.creditconveyer.deal.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value(value = "${spring.kafka.bootstrapAddress}")
+    private String bootstrapAddress;
+
+    @Bean
+    public ProducerFactory<String, PaymentResponseDto> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, PaymentResponseDto> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
