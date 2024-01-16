227c4e535cfbbaa757c2b19981bd1b86f4d69145
vivannikov1 <vivannikov@neoflex.ru>
2024-01-16 10:33:57 +0300

    write config files

--- gateway/src/main/java/com/neoflex/creditconveyer/gateway/GatewayApp.java
+++ gateway/src/main/java/com/neoflex/creditconveyer/gateway/GatewayApp.java
@@ -2,8 +2,10 @@ package com.neoflex.creditconveyer.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class GatewayApp {
 
     public static void main(String[] args) {
