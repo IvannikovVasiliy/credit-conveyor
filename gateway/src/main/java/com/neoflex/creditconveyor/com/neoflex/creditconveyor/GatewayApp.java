0b1fb7a9454c5f34e70ebffa050b2a0a9628bba1
vivannikov1 <vivannikov@neoflex.ru>
2024-01-17 13:49:40 +0300

    do without config server

--- gateway/src/main/java/com/neoflex/creditconveyor/com/neoflex/creditconveyor/GatewayApp.java
+++ gateway/src/main/java/com/neoflex/creditconveyor/com/neoflex/creditconveyor/GatewayApp.java
@@ -1,11 +1,13 @@
 package com.neoflex.creditconveyor.com.neoflex.creditconveyor;
 
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class GatewayApp {
 
     public static void main(String[] args) {
