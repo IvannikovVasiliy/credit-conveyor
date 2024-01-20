12754ce82fa3cde31307b9d4b89be1514bfd1d8f
vivannikov1 <vivannikov@neoflex.ru>
2024-01-20 16:01:38 +0300

    change serializers from long to string

--- deal/src/main/java/com/neoflex/creditconveyer/deal/config/KafkaProducerConfig.java
+++ deal/src/main/java/com/neoflex/creditconveyer/deal/config/KafkaProducerConfig.java
@@ -5,6 +5,7 @@ import com.neoflex.creditconveyer.deal.domain.dto.EmailMessage;
 import com.neoflex.creditconveyer.deal.domain.dto.SesEmailMessage;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,10 +24,10 @@ public class KafkaProducerConfig {
     private String bootstrapAddress;
 
     @Bean
-    public ProducerFactory<Long, EmailMessage> emailMessageProducerFactory() {
+    public ProducerFactory<String, EmailMessage> emailMessageProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
@@ -41,26 +42,26 @@ public class KafkaProducerConfig {
 //    }
 
     @Bean
-    public ProducerFactory<Long, InformationEmailMessage> emailCreditMessageProducerFactory() {
+    public ProducerFactory<String, InformationEmailMessage> emailCreditMessageProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         configProps.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 500);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public ProducerFactory<Long, SesEmailMessage> emailSesCodeMessageProducerFactory() {
+    public ProducerFactory<String, SesEmailMessage> emailSesCodeMessageProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<Long, EmailMessage> emailKafkaTemplate() {
+    public KafkaTemplate<String, EmailMessage> emailKafkaTemplate() {
         return new KafkaTemplate<>(emailMessageProducerFactory());
     }
 
@@ -70,12 +71,12 @@ public class KafkaProducerConfig {
 //    }
 
     @Bean
-    public KafkaTemplate<Long, InformationEmailMessage> creditEmailKafkaTemplate() {
+    public KafkaTemplate<String, InformationEmailMessage> creditEmailKafkaTemplate() {
         return new KafkaTemplate<>(emailCreditMessageProducerFactory());
     }
 
     @Bean
-    public KafkaTemplate<Long, SesEmailMessage> emailSesCodeKafkaTemplate() {
+    public KafkaTemplate<String, SesEmailMessage> emailSesCodeKafkaTemplate() {
         return new KafkaTemplate<>(emailSesCodeMessageProducerFactory());
     }
 }
