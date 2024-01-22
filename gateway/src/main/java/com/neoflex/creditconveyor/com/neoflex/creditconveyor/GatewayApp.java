a826fdbaef232c36402b13e335e0c8ac14803a16
vivannikov1 <vivannikov@neoflex.ru>
2024-01-22 15:46:13 +0300

    configure annotations

--- gateway/src/main/java/com/neoflex/creditconveyor/com/neoflex/creditconveyor/GatewayApp.java
+++ gateway/src/main/java/com/neoflex/creditconveyor/com/neoflex/creditconveyor/GatewayApp.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,4 +16,16 @@ public class GatewayApp {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApp.class, args);
     }
+
+//    @Bean
+//    public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
+//        return routeLocatorBuilder
+//                .routes()
+//                .route(route -> route
+//                        .path("/deal/**")
+//                        .uri("http://localhost:8081")
+//
+//                )
+//                .build();
+//    }
 }
