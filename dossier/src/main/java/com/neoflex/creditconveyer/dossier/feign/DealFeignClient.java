9ea35530dd28e2d9ef4c069d53396095b2510658
vivannikov1 <vivannikov@neoflex.ru>
2024-01-29 15:07:40 +0300

    add @Validated

--- dossier/src/main/java/com/neoflex/creditconveyer/dossier/feign/DealFeignClient.java
+++ dossier/src/main/java/com/neoflex/creditconveyer/dossier/feign/DealFeignClient.java
@@ -3,10 +3,12 @@ package com.neoflex.creditconveyer.dossier.feign;
 import com.neoflex.creditconveyer.dossier.config.DecoderConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @FeignClient(value = "dealFeign", url = "${dealService.hostPort}/v2/deal", configuration = DecoderConfiguration.class)
+@Validated
 public interface DealFeignClient {
 
     @PutMapping("/admin/application/{applicationId}/status")
